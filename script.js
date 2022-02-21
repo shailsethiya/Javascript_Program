2a53dcfc50312aae6e7e6b0eda68e4cf3059d6a8
shailendra <shailendrasethiya@gmail.com>
2022-02-21 20:41:52 +0530

    reverse word

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const abc = "I am boy";
-document.write("   string====", abc);
-const reverseString = abc.split(" ").reverse().join(" ");
-document.write("  reverseString====", reverseString);
+const abc = "shailendra";
+document.write("string====", abc);
+const reverseString = abc.split("").reverse().join("");
+document.write("reverseString====", reverseString);

465afa34463cd30327ecd0db715b43fa76819cfc
shailendra <shailendrasethiya@gmail.com>
2022-02-21 20:57:45 +0530

    array rotate

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const abc = "shailendra";
-document.write("string====", abc);
-const reverseString = abc.split("").reverse().join("");
-document.write("reverseString====", reverseString);
+const rotate = (arr, count = 1) => {
+    return [...arr.slice(count, arr.length), ...arr.slice(0, count)];
+};
+  
+const arr = [1,2,3,4,5];
+
+console.log(rotate(arr, 1));  // [2, 3, 4, 5, 1]
+console.log(rotate(arr, 2));  // [3, 4, 5, 1, 2]
+console.log(rotate(arr, -2)); // [4, 5, 1, 2, 3]
+console.log(rotate(arr, -1)); // [5, 1, 2, 3, 4]

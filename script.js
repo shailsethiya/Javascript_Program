fe4528f803d8d189b21ba6079b4871d2e215b69c
shailendra <shailendrasethiya@gmail.com>
2022-02-22 23:56:12 +0530

    palindrome_no

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// program to find the factorial of a number
-
-function factorial(n){
-// checking if number is negative
-if (n < 0) {
-    console.log('Error! Factorial for negative number does not exist.');
-}
-
-// if number is 0
-else if (n === 0) {
-    console.log(`The factorial of ${n} is 1.`);
-}
-
-// if number is positive
-else {
-    let fact = 1;
-    for (i = 1; i <= n; i++) {
-        fact *= i;
-    }
-    alert(`The factorial of ${n} is ${fact}.`);
-}
-}
-
-factorial(5);
+function palindrome(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    alert(reverseStr === lowRegStr);
+    return reverseStr === lowRegStr;
+  }
+  palindrome("A man, a plan, a canal. Panama");

4ca427eb976fa470ae7e7785d76815573427313d
shailendra <shailendrasethiya@gmail.com>
2022-02-22 23:57:36 +0530

    palindrome no

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function palindrome(str) {
-    var re = /[\W_]/g;
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
-    alert(reverseStr === lowRegStr);
-    return reverseStr === lowRegStr;
-  }
-  palindrome("A man, a plan, a canal. Panama");
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
+   palindrome("A man, a plan, a canal. Panama");

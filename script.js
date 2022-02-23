6b341156844600b5909f526d48bd7949ef3c054c
shailendra <shailendrasethiya@gmail.com>
2022-02-23 18:16:17 +0530

    setTimeout

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let arr = [[1,2], [1,4]]; 
-
-let flattenArr = [].concat(...arr);
-
-console.log(flattenArr); // [1,2,3,4]
-
-
-let arr1 = [[1,2], [3,4], [5,6, [7, 8], 9], [10, 11, 12],];
-
-console.log(arr1.flat(2)); //[1,2,3,4,5,6,7,8,9,10,11,12]
-
-
-function customFlat(arr1, depth = 2){
-  let result = [];
-  arr1.forEach((ar) => {
-    if(Array.isArray(ar) && depth > 0){
-      result.push(...customFlat(ar, depth-1));
-    } else result.push(ar);
-  });
-
-  return result;
+function a(){
+  for(var i =0; i<3; i++){
+    setTimeout(function log(){
+      console.log(i);
+    }, i * 1000)
+  }
 }
 
-console.log(customFlat(arr1));
+// 3 
+// 3
+// 3
+
+a();
+
+
+function a(){
+  for(let i =0; i<3; i++){
+    setTimeout(function log(){
+      console.log(i);
+    }, i * 1000)
+  }
+}
+
+// 0 
+// 1
+// 2
+
+a();

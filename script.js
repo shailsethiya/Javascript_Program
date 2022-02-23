8c6ee01f8157915918f74327391c74386f5c3944
shailendra <shailendrasethiya@gmail.com>
2022-02-23 11:09:02 +0530

    pyramid

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function secondMax() {
-  var arr = [20, 120, 111, 215, 54, 78]; // use int arrays
-  var max = Math.max.apply(null, arr); // get the max of the array
-  arr.splice(arr.indexOf(max), 1); // remove max from the array
-  console.log(Math.max.apply(null, arr));
-  return Math.max.apply(null, arr); // get the 2nd max
+function generatePyramid() {
+  var totalNumberofRows = 5;
+  var output = '';
+  for (var i = 1; i <= totalNumberofRows; i++) {
+      for (var j = 1; j <= i; j++) {
+          output += j + '  ';
+      }
+      console.log(output);
+      output = '';
+  }
 }
 
-secondMax();
+generatePyramid();

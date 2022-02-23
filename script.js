3d1cfe0067585df3f9a3ec6372ec2871199729bb
shailendra <shailendrasethiya@gmail.com>
2022-02-23 11:03:04 +0530

    second largest no

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function secondMax() {
-  const arr = [20, 120, 111, 215, 54, 78]; // use int arrays
-  const min = Math.min.apply(null, arr); // get the max of the array
-  arr.splice(arr.indexOf(min), 1); // remove max from the array
-  console.log(Math.min.apply(null, arr));
-  return Math.min.apply(null, arr); // get the 2nd max
+  var arr = [20, 120, 111, 215, 54, 78]; // use int arrays
+  var max = Math.max.apply(null, arr); // get the max of the array
+  arr.splice(arr.indexOf(max), 1); // remove max from the array
+  console.log(Math.max.apply(null, arr));
+  return Math.max.apply(null, arr); // get the 2nd max
 }
 
 secondMax();

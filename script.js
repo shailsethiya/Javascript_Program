078a8d72d890d5439e4727795a4a40d6108ebda2
shailendra <shailendrasethiya@gmail.com>
2022-02-23 21:17:22 +0530

    calc code

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function add(a){
-    return function (b){
-        if(b) return add (a + b );
-        return a;
-    };
+const calc = {
+    total:  0,
+    add(a) {
+        this.total += a;
+        return this;
+    },
+    mul(a) {
+        this.total *= a;
+        return this;
+    },
+    sub(a) {
+        this.total -= a;
+        return this;
+    },
 }
 
-console.log(add(5)(2)(3)(7)());
+const result = calc.add(10).mul(5).sub(30).add(10);
+
+console.log(result.total)

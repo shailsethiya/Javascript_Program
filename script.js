042a827704e0f2350b2f2d62c5298f83a26eaf25
shailendra <shailendrasethiya@gmail.com>
2022-02-23 20:17:56 +0530

    hosting

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function abc(){
+    console.log(a); // undefined 
+    console.log(b); // reference error 
+    console.log(c); // reference error 
 
+    //let & const variable hosted in temporal dead zone  
+    const c = 30;
+    let b = 20;
+    var a = 10;
+}
+
+abc();

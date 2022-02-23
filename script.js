ca0476782454b0c6d3da8a51a89de550460fc8a7
shailendra <shailendrasethiya@gmail.com>
2022-02-23 10:09:13 +0530

    largest no

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function findlargestNowWithMethod() {
-  const largestNo = Math.max(3,4,5,6,7,9);
+  const arr = [3,4,5,6,7,9];
+  const largestNo = Math.max(...arr);
   console.log(largestNo);
 }
 

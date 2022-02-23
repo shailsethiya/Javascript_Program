abdaf7d4f008782293e5d3479bfefbf4179d670a
shailendra <shailendrasethiya@gmail.com>
2022-02-23 21:04:33 +0530

    infinite currying

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function abc(){
-    console.log(a); // undefined 
-    console.log(b); // reference error 
-    console.log(c); // reference error 
-
-    //let & const variable hosted in temporal dead zone  
-    const c = 30;
-    let b = 20;
-    var a = 10;
+function add(a){
+    return function (b){
+        if(b) return add (a + b );
+        return a;
+    };
 }
 
-abc();
+console.log(add(5)(2)(3)(7)());

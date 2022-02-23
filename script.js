d7573d4ac2b8a59135d295ca9c8477be865caf0f
shailendra <shailendrasethiya@gmail.com>
2022-02-23 11:00:04 +0530

    second smallest no

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function findsmallestNowWithoutMethod() {
-  let array = [3 , 6, 2, 56, 32, 5, 89, 32];
-  let smallest= array[0];
-  
-  for (i=0; i<=array.length;i++){
-      if (array[i] < smallest) {
-        smallest=array[i];
-      }
-  }
-  console.log(smallest);
+function secondMax() {
+  const arr = [20, 120, 111, 215, 54, 78]; // use int arrays
+  const min = Math.min.apply(null, arr); // get the max of the array
+  arr.splice(arr.indexOf(min), 1); // remove max from the array
+  console.log(Math.min.apply(null, arr));
+  return Math.min.apply(null, arr); // get the 2nd max
 }
 
-findsmallestNowWithoutMethod();
+secondMax();

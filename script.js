c902c01a04662946529c27124ae72594ada1fea8
shailendra <shailendrasethiya@gmail.com>
2022-02-24 12:30:26 +0530

    array empty

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let arr = [1,2,3,4,5];
+arr = [] // first way
+console.log(arr);
+
+let arr = [1,2,3,4,5];
+arr.length = 0; // second way
+console.log(arr);
+
+let arr = [1,2,3,4,5];
+arr.splice(0, arr.length) // third way
+console.log(arr);
+
+let arr = [1,2,3,4,5];
+while(arr.length > 0)
+arr.pop();
 

b0be5ce3c54b62c065605c9c26e92006eacfc78a
shailendra <shailendrasethiya@gmail.com>
2022-02-24 13:19:06 +0530

    function

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+function foo() {
+    return bar();
+      var bar = function(){
+          console.log('bar');  // bar is not a function
+      }
+  
+  }
+  
+  foo();
 
+/*================================================================================*/
+  
+function foo() {
+    var bar = function(){
+        console.log('bar');  // bar 
+    }
+      return bar();
+
+}
+
+foo();
+
+/*===============================================================================*/
+
+function foo(){
+    return bar();
+    function bar() {
+        console.log('bar');  // bar
+    }
+}
+
+foo();
+
+/*================================================================================*/
+
+function foo(){
+    function bar() {
+        console.log('bar');  // bar
+    }
+    return bar();
+}
+
+foo();

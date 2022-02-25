48900a311f3419dbbcadab3fce83086ea07328ba
shailendra <shailendrasethiya@gmail.com>
2022-02-25 20:04:38 +0530

    dublicate

--- script.js
+++ script.js
@@ -29,9 +29,25 @@ const sampleArray = ["pune", "jaipur", "pune", "mumbai", "mumbai"];
 sampleArray.forEach(function (x) { counts[x] = (counts[x] || 0) + 1;console.log(counts, counts[x])});
 console.log(counts);  
 
-{pune : 2, jaipur : 1, mumbai: 2};
+// {pune : 2, jaipur : 1, mumbai: 2};
 
 // Explanation - 
 // We call forEach on arr with a callback that puts the array item x as a property of count .
 // Then we update counts[x] as the item x is being found from iteration.
 // If count[x] isn’t set yet, then 0 is set as the value of count[x] .
+
+
+/************************************************************************************** */
+
+
+//how to count specific value duplicity in array object
+
+const counts = {};
+const names = [{id:1,fName:'Shailendra',lName:'Sethiya', age:20},
+{id:2,fName:'Shailendra',lName:'Sethiya', age:25},
+{id:1,fName:'Shailendra',lName:'Sethiya', age:25}]
+
+names.forEach(function (x) { counts[x.age] = (counts[x.age] || 0) + 1;});
+console.log(counts)
+
+// output - {20: 1, 25: 2}

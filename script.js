b020065aca6521f685c3e066ba692e817b9a4171
shailendra <shailendrasethiya@gmail.com>
2022-02-25 18:29:46 +0530

    this keyword

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+const a = function(){
+    console.log(this);   // window object 
+   
+    const b = {
+      func1: function(){
+        console.log(this);  // function 
+      }
+    }
+  
+    const c = {
+      func2: ()=>{
+        console.log(this); // window object
+      }
+    }
+  
+    b.func1();
+    c.func2();
+  }
+  
+  a();

3ed308c7ff3ce9ec1be09d1f10c139ace9da48e5
shailendra <shailendrasethiya@gmail.com>
2022-02-25 14:23:08 +0530

    hosting

--- script.js
+++ script.js
@@ -10,3 +10,52 @@ function abc(){
 }
 
 abc();
+
+
+/*=============================================================================*/
+//because of hosting it will decalre the value of x
+
+var x = 10; 
+function test() {
+     console.log(x); // undefined 
+      var x = 20; 
+};
+ test();
+
+/*===================================================================================*/
+
+for(var i =0; i<=10; i++) {
+    setTimeOut(()=>{
+    console.log(i); // 11 print 10 time(bind only one times so last value refer till the loop end). 
+    },2000);
+    }
+    
+    console.log(i); // 11
+
+/*======================================================================================*/
+   
+for(let i =0; i<=10; i++) {
+    setTimeOut(()=>{
+    console.log(i); // 0-10 print  (every iteration new value bind )
+    },2000);
+    }
+        
+    console.log(i); // 11    
+
+ /*=====================================================================================*/   
+
+ for(var i =0; i<=10; i++) { 
+    console.log(i);  // 0 - 10 value 
+    } 
+    
+    console.log(i); // 11
+
+/*=====================================================================================*/
+
+for(let i = 0; i<=10; i++) { 
+    console.log(i);  // 0 - 10
+    } 
+    
+    console.log(i); // not defined
+
+ /*====================================================================================*/   

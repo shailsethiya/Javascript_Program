be47f1e730a2dbfbbaf24c98d19c0cc21e4ae303
shailendra <shailendrasethiya@gmail.com>
2022-02-25 15:25:53 +0530

    string rev

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const a = [1,2,3,1,2,9];
+var str = "get busy living or get busy dying.";
+var result = str.split(" ").map(function(word, i) {
+  return i % 2 == 0 ? word : word.split("").reverse().join(""); }).join(" ");
 
-const b = [... new Set(a)];  // find Array dublicate 
-
-console.log(b);
-
-==================================================================================
-
-const arr = [1,2,3,1,2,9];
-
-let arr1 = [];
-
-let len = arr.length;
-
-for(let i = 0; i < len; i++){
-    if(arr1.indexOf(arr[i]) === -1){
-        arr1.push(arr[i])
-    }
-}
-
-console.log(arr1);
+alert(result);

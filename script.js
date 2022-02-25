4ba0a3c400d12aca7679ba96cfcad6fda2a98bf3
shailendra <shailendrasethiya@gmail.com>
2022-02-25 18:36:17 +0530

    func output

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ alert("property exist");
 alert("property doesn't exist");   //   property doesn't exist
 
 }
+
+
+/***********************************************************************/
+
+// var, let & const 
+var a = {
+    firstName: 'firstPersonName'
+  }
+  var b = a;
+  b.firstName = 'secondPersonName';
+  console.log(a.firstName);   // secondPersonName
+
+
+  /*************************************************************************/
+
+  //How to get value of specific key from array object
+
+    const arr = [{name: 'Person1', age: 26}, {name: 'Person2', age: 34}];
+    arr.map((value) => value.name);
+
+    Output-  ['Person1', 'Person2']

7ce4311a7c609fea2eabccd478a28c4f17373c77
shailendra <shailendrasethiya@gmail.com>
2022-02-28 18:50:32 +0530

    closure

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//Closure
+
+//A Closure bind together with it's lexical envirnoment 
+
+// function along with lexical scope form a closure
+
 function a(){
     let a = 11;
     function b(){
@@ -6,3 +12,53 @@ function a(){
     b();
 }
 a();
+
+/****************************************************************************/
+
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a); // 7 
+    }
+    return y;
+}
+
+var z = x();
+console.log(z); // y(){ console.log(a); }
+
+//....
+
+z();
+
+/*************************************************************************************** */
+
+function x(){
+    var a = 7;
+    function y(){
+        console.log(a); // 100
+    }
+    a = 100;
+    return y;
+}
+
+var z = x();
+console.log(z); // y(){ console.log(a); }
+
+//....
+
+z()
+
+/******************************************************************************************/
+
+function z() {
+    var b = 900;
+    function x() {
+        var a = 4;
+      function y(){
+          console.log(a, b);  // 4, 900
+      }  
+      y(); 
+    }
+    x();
+}
+z();

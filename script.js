881b190745bb55844022859a69e1953369d4bd7e
shailendra <shailendrasethiya@gmail.com>
2022-02-28 19:57:36 +0530

    settime out

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// closure 
+// function remember the reference of i, so all of them copy same reference of i 
 function a(){
   for(var i =0; i<3; i++){
-    setTimeout(function log(){
+    setTimeout(function log(){ 
       console.log(i);
     }, i * 1000)
   }
@@ -12,11 +14,13 @@ function a(){
 
 a();
 
+/*************************************************************************** */
 
+// let is block level scope
 function a(){
   for(let i =0; i<3; i++){
     setTimeout(function log(){
-      console.log(i);
+      console.log(i);  // create a new copy of i each time (new reference of i) always different memory location
     }, i * 1000)
   }
 }
@@ -26,3 +30,19 @@ function a(){
 // 2
 
 a();
+
+/******************************************************************************************** */
+
+// work with var, it's work because it's create a new copy of i 
+function a(){
+    for(var i =0; i<3; i++){
+       function close(i){
+        setTimeout(function log(){
+            console.log(i); 
+          }, i * 1000)
+       } 
+       close(i);
+    }
+  }
+  
+  a();

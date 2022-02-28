cb7385e250392fc8984a59db0ea33e1f55f6fa33
shailendra <shailendrasethiya@gmail.com>
2022-02-28 21:15:23 +0530

    first class functiom

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-// closure 
-// function remember the reference of i, so all of them copy same reference of i 
+//function Statement && function declaration 
 function a(){
-  for(var i =0; i<3; i++){
-    setTimeout(function log(){ 
-      console.log(i);
-    }, i * 1000)
-  }
+    console.log(a);
 }
-
-// 3 
-// 3
-// 3
-
 a();
 
-/*************************************************************************** */
-
-// let is block level scope
-function a(){
-  for(let i =0; i<3; i++){
-    setTimeout(function log(){
-      console.log(i);  // create a new copy of i each time (new reference of i) always different memory location
-    }, i * 1000)
-  }
+//function Expression 
+var a = function (){
+    console.log(a);
 }
-
-// 0 
-// 1
-// 2
-
 a();
 
-/******************************************************************************************** */
+// first class function or first class citizen-- 
+// the ability to use function as a value or pass the function as a paramater  is called first class function
 
-// work with var, it's work because it's create a new copy of i 
-function a(){
-    for(var i =0; i<3; i++){
-       function close(i){
-        setTimeout(function log(){
-            console.log(i); 
-          }, i * 1000)
-       } 
-       close(i);
+var b = function(param1){
+    return function xyz() {
+
     }
-  }
-  
-  a();
+}
+
+console.log(b());

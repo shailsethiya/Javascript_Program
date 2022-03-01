99e0021ad5cbec0193c3084f3d545e3055c5e467
shailendra <shailendrasethiya@gmail.com>
2022-03-01 23:24:14 +0530

    function curry

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+let multiply = function (x, y){
+    console.log(x * y); 
+}
+
+let multiplyByTwo =  multiply.bind(this, 2);
+multiplyByTwo(5);
+
+let multiplyByThree =  multiply.bind(this, 3);
+multiplyByTwo(5);
+
+
+// by using bind or closure we can do function curry
+
+let multiply = (x) => {
+   return (y) => {
+        console.log(x * y);
+    }
+}
+
+let multiplyByTwo = multiply(2);
+multiplyByTwo(3);

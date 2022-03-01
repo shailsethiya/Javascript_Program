99ca8e5b52fd2a3a8c3a61642c9441af33064a9a
shailendra <shailendrasethiya@gmail.com>
2022-03-01 20:26:07 +0530

    stop

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+document.querySelector("#grandparent").addEventListener('click', () => {
+    console.log("grand parent");
+}, true) // capturing 
+
+document.querySelector("#parent").addEventListener('click', () => {
+    console.log("parent");
+}, false) // bubbling 
+
+document.querySelector("#child").addEventListener('click', () => {
+    console.log("child");
+}, true) // capturig 
+
+
+/* grand parent 
+   child
+   parent
+*/
+
+/*********************************************************************************** */
+
+document.querySelector("#grandparent").addEventListener('click', () => {
+    console.log("grand parent");
+}, false) 
+
+document.querySelector("#parent").addEventListener('click', (e) => {
+    console.log("parent");
+    e.stopPropagation();
+}, false) // stop propogation 
+
+document.querySelector("#child").addEventListener('click', () => {
+    console.log("child");
+}, false) 

e9f8fbe2f794e4ccbcf5f05ba07100f2fbc29503
shailendra <shailendrasethiya@gmail.com>
2022-03-01 09:39:00 +0530

    promise

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ resolve();
 }).then(() => {
     console.log("do this 2");
 });
+
+/**************************************************************************************** */
+
+Promise.resolve().then(() => console.log(2));
+console.log(1); // 1,2 
+
+
+/**************************************************************************************** */
+
+// Promise.all() - Wait for all promises to be resolved, or for any to be rejected.
+// Promise.allSettled() - Wait until all promises have settled (each may resolve or reject).

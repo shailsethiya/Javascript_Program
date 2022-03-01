868d59521349f53c5ad38463435a1094cee49f91
shailendra <shailendrasethiya@gmail.com>
2022-03-01 21:09:35 +0530

    event delegation

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+document.querySelector("#category").addEventListener('click', (e) => {
+  console.log(e.target);
+  if(e.target.tagName == 'L1'){
+      window.location.href = "/" + e.target.id;
+  }
+});

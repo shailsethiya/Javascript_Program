d898ec18917b0d90f52c6068f4284e6aab938e54
shailendra <shailendrasethiya@gmail.com>
2022-03-02 10:08:09 +0530

    array object

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+listOfnumbers = [3,4,5,6,7,8,9,10];
+
+listOfnumbers.map((x) => {
+    const obj = { };
+    obj['evenOrOdd'] = (x%2 === 0) ? 'even' : 'odd';
+    obj['areOfCircle'] =  Math.PI*x*x;
+    return obj;
+});

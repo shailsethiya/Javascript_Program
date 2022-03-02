14d3ba5cc111cb4bfcc99b229b1ef2aa50c4b45c
shailendra <shailendrasethiya@gmail.com>
2022-03-02 09:46:30 +0530

    sorting

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+const listOfPerson = [
+    {name:'Aditya', dateOfBirth:"10/09/1982", aadharCardNumber: '1234-2323-1233-7657',address:'Bangaluru'},
+    {name:'Kriti', dateOfBirth:"10/09/2019", aadharCardNumber: '1234-1010-1233-7657',address:'Chennai'},
+    {name:'Binod', dateOfBirth:"01/09/1990", aadharCardNumber: '4523-1010-1233-7657',address:'Bhopal'},
+    {name:'Amar', dateOfBirth:"01/09/1997", aadharCardNumber: '4523-1010-1233-2888',address:'Ajmer'}
+];
+
+    
+listOfPerson.sort((a,b) => a.name > b.name ? 1 : -1);

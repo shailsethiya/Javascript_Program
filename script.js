41d3b097e2224dc6ebf47c5cab4c62c617074f70
shailendra <shailendrasethiya@gmail.com>
2022-03-16 22:48:43 +0530

    added

--- script.js
+++ script.js
@@ -103,3 +103,25 @@ console.log(obj)
 Output - {20: 2, 25:3}
 
 /********************************************************************************************** */
+
+//to find 1st repeated occurrence in array elements
+
+function nonRepeated(arr) {
+// created empty object
+const obj = {};
+
+for (let user of arr) {
+		obj[user] = obj[user] + 1 || 1;
+}
+// will get array element with count for repeated value
+console.log(obj); // { 1: 1, 2: 2, 3: 1 }
+for (let elem in obj) {
+// return key which value is not equal to 1
+	if(obj[elem] !== 1) {
+		return elem;
+	}
+}
+	return -1;
+}
+
+console.log(nonRepeated([1,2,3,2])); // 2

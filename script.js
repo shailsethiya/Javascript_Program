24ea16f20f79e3f980d991da3bc5a75f79fceb2b
shailendra <shailendrasethiya@gmail.com>
2022-03-16 18:42:53 +0530

    added

--- script.js
+++ script.js
@@ -31,3 +31,41 @@ var a = {
     arr.map((value) => value.name);
 
     Output-  ['Person1', 'Person2']
+
+    /****************************************************************** */
+
+    console.log(a); // undefined
+    var a= 9;
+    function Foo()
+    {  console.log(a); // undefined
+      var a = 99;
+      console.log(a); // 99
+    }
+    Foo();
+    console.log(a); // 9
+
+    /********************************************************************* */
+
+    var myObject = {
+        foo: "bar",
+        func: function() {
+            var self = this;
+            console.log("outer func:  this.foo = " + this.foo); //bar
+            console.log("outer func:  self.foo = " + self.foo); //bar
+            (function() {
+                console.log("inner func:  this.foo = " + this.foo);  // undefined
+                console.log("inner func:  self.foo = " + self.foo);  // bar
+            }());
+        }
+    };
+    myObject.func();
+
+    /**************************************************************************** */
+
+    for(var i=0;i<5;i++){
+        setInterval(console.log(i),0)
+      };
+
+    //Output - 0,1,2,3,4
+
+    /****************************************************************************** */

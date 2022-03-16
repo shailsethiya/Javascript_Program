e9b2eee3d28a5b27f55ec10b0c2fd3e18d64528c
shailendra <shailendrasethiya@gmail.com>
2022-03-16 18:55:58 +0530

    this keyword

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ const a = function(){
   }
   
   a();
+
+  /***************************************************************************8 */
+
+  var myObject = {
+    foo: "bar",
+    func: function() {
+        var self = this;
+        console.log("outer func:  this.foo = " + this.foo);
+        console.log("outer func:  self.foo = " + self.foo);
+        (function() {
+            console.log("inner func:  this.foo = " + this.foo);
+            console.log("inner func:  self.foo = " + self.foo);
+        }());
+    }
+};
+myObject.func();
+
+// Output -
+// outer func:  this.foo = bar
+// outer func:  self.foo = bar
+// inner func:  this.foo = undefined
+// inner func:  self.foo = bar
+
+/********************************************************************** */

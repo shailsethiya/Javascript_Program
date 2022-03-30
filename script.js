da2868f40598688b023a0f64306766153735fffd
shailendra <shailendrasethiya@gmail.com>
2022-03-30 23:42:55 +0530

    map

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+// Map, Filter and Reduce
+// Polyfill for map()
 
+
+Array.prototype.myMap = function(cb){
+   let temp = [];
+   for (let i =0; i< this.length; i++){
+     temp.push(cb(this[i], i, this));
+   }
+   return temp;
+};
+
+
+// Polyfill for filter()
+
+Array.prototype.myFilter = function (cb){
+  let temp = [];
+  for(let i=0; i< this.length; i++){
+    if(cb(this[i], i, this))
+    temp.push(this[i]);
+  }
+  return temp;
+}
+
+
+// polyfill for reduce()
+
+Array.prototype.myReduce = function(cb, initialValue){
+  var accumulator = initialValue;
+
+  for(let i =0; i< this.length; i++){
+    accumulator = accumulator? cb(accumulator, this[i], i, this) : this[i];
+  }
+
+  return  accumulator;
+};

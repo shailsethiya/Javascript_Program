55858d2a2fb9c3a29c9d581533de965d13f79792
shailendra <shailendrasethiya@gmail.com>
2022-03-30 21:03:31 +0530

    deep copy

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let obj = {
+  firstName: "shailendra",
+  lastName: "sethiya",
+  address: {
+    cityName: "indore",
+    pincode: 458441
+  },
+  mobile: [25, 26]
+};
 
+function deepCopy(obj) {
+  let ob = {};
+  for (let key in obj) {
+    if (Array.isArray(obj[key])) {
+      ob[key] = [...obj[key]];
+    } else if (obj[key] instanceof Object) {
+      ob[key] = deepCopy(obj[key]);
+    } else {
+      ob[key] = obj[key];
+    }
+  }
+  return ob;
+}
+
+const obj2 = deepCopy(obj);
+console.log(obj2);

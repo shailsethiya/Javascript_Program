c6797a2e96781f4b76955b73ccc19a7124d1a0c4
shailendra <shailendrasethiya@gmail.com>
2022-03-30 20:51:51 +0530

    squr_root

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let n = 3; // height of pattern
+let string = '';
+// External loop
+for (let i = 1; i <= n; i++) {
+  // Internal loop
+  for (let j = 1; j <= i; j++) {
+    string += j*j;
+  }
+	string += "\n";
+}
+console.log(string);
 
+Output -
+1
+1 4
+1 4 9

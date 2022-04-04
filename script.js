63eff119c8b6e25b5b7247fa2dcef6311679212b
shailendra <shailendrasethiya@gmail.com>
2022-04-04 12:19:26 +0530

    array

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let initialArray = [2, 3, 4, 5, 6, 4, 3, 5, 5];
 
-initialArray.reduce(function(result, value, index, array) {
+initialArray.reduce((result, value, index, array) => {
   if (index % 2 === 0)
     result.push(array.slice(index, index + 2));
   return result;

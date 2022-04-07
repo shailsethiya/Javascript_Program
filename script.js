a1869b9be05156b4d78566ab04df39b42185af84
shailendra <shailendrasethiya@gmail.com>
2022-04-07 16:01:35 +0530

    algo

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let a = [1, 4, 5, 8, 3, 2, 6, 9, 7, 10];
+a.sort((a, b) => a - b);
+let b =[];
+    
+let l = a.length-1;  // micro optimization
+let L = l/2;         // micro optimization
+for(var i=0; i<L; i++) b.push( a[l-i] ,a[i] );
+if(a.length%2) b.push( a[i] ); // add last item in odd arrays
 
+console.log(b);
+
+b =  [10, 1, 9, 2, 8, 3, 7, 4, 6, 5]
